a987b3fdfc2ac36bb4006dc7f3853e10a14bca8f
moses <mosesk394@gmail.com>
2015-02-25 15:15:08 +0200

    add personal greeting

---
+++ thut/hellocpp.cpp
@@ -0,0 +1,24 @@
+//our first c++ program(hello,world)
+//moses maseko,cm3,tut1 25/02/2015
+#include<iostream>
+using namespace std;
+#include<string>
+
+int main()
+{
+
+	string myName;
+	cout << "please enter your name:";
+	cin >> myName;
+
+
+
+	cout << "hello , " << myName<<endl;
+
+
+
+
+	system("pause");
+
+
+}
